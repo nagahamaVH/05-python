5abe3da299a11aba7164eaf07021322190f57677
nataliaasaa <nataliaasaa@gmail.com>
2020-08-18 16:39:42 -0300

    Exercícios de 01 a 19 da lista da Aula 01

---
+++ solaces/ex4.py
@@ -0,0 +1,4 @@
+x = 4
+y = x + 1   #y=5
+x = 2       #novo valor para x
+print(x, y) #x=2, y=5
---
+++ solaces/ex8.py
@@ -0,0 +1,6 @@
+x = 1
+def f():
+	return x
+print(x) #retorna x, que é = 1
+print(f()) #retorna 1
+#A função f() retorna x que é =1
